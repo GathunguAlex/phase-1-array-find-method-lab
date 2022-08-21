bcb5e517cbb9328c62e9f89441aa78af4e4ff799
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-08-22 02:09:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+let superbowlWin(result)[
+ { year: 2015, result: 'win' },
+ { year: 2014, result: 'n/a' },
+{ year: 2013, result: 'lose' },
+
+return console.log superbowlWin.result === 'win';
+
+else { console.log('sadly') };
+]
+
+ 
+
